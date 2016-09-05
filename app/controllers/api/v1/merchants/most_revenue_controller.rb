a25e17a69754143b8a764bc4a28c84e22a9e39d7
Kris Sparks <kbs5280@gmail.com>
2016-09-05 15:51:53 -0600

    Extracted merchant logic to model for most revenue.
    Model method is not being called when the controller is hit from Postman. Not
    sure how to move forward. Hopefully we'll cover this in class tomorrow.

--- app/controllers/api/v1/merchants/most_revenue_controller.rb
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::MostRevenueController < ApplicationController
   def index
-    Merchant.joins(invoices: [:transactions, :invoice_items]).where(transactions: { result: 'success' }).group(:id).order("sum(invoice_items.quantity * invoice_items.unit_price) DESC")
+    Merchant.most_revenue(params[:quantity])
   end
 end
