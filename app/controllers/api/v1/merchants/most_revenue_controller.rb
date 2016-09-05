32b7fb02f115858a4832f9b165e7a894d64800a0
Kris Sparks <kbs5280@gmail.com>
2016-09-05 15:27:16 -0600

    Most revenue logic in place
    Need to verify results. Need to extract logic from controller and add to model. Need
    to interpolate 'quantity' param.

--- app/controllers/api/v1/merchants/most_revenue_controller.rb
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::MostRevenueController < ApplicationController
   def index
-    # GET /api/v1/merchants/most_revenue?quantity=x returns the top x merchants ranked by total revenue
+    Merchant.joins(invoices: [:transactions, :invoice_items]).where(transactions: { result: 'success' }).group(:id).order("sum(invoice_items.quantity * invoice_items.unit_price) DESC")
   end
 end
