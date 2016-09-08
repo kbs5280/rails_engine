1b97000237a48bfe61566111eb96130ccaabcc5a
Lane Winham <mlwinham12@gmail.com>
2016-09-07 20:38:49 -0600

    Add relationship endpoints for merchant and invoices

--- app/controllers/api/v1/invoices/transactions_controller.rb
+++ app/controllers/api/v1/invoices/transactions_controller.rb
@@ -3,8 +3,6 @@ class Api::V1::Invoices::TransactionsController < ApplicationController
 
   def index
     transactions = Invoice.find(params[:id]).transactions
-    # transactions = Invoice.joins(:transactions).find(params[:id])
-    # transactions = Invoice.joins(:transactions).where(invoice_id: params[:id])
     respond_with transactions
   end
 end
