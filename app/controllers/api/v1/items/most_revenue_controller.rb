b9c4b8078c0d17fb41c8b1567b34e780781181ea
Lane Winham <mlwinham12@gmail.com>
2016-09-08 16:58:58 -0600

    Pull method for most revenue for item into model, not passing

--- app/controllers/api/v1/items/most_revenue_controller.rb
+++ app/controllers/api/v1/items/most_revenue_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::Items::MostRevenueController < ApplicationController
   respond_to :json, :xml
 
   def index
-    most_revenue = Item.joins(:invoice_items).group(:id).order("sum(invoice_items.quantity * invoice_items.unit_price) DESC").limit(params[:quantity])
+    # most_revenue = Item.joins(:invoice_items).group(:id).order("sum(invoice_items.quantity * invoice_items.unit_price) DESC").limit("#{quantity}")
+    most_revenue = Item.joins(:invoice_items)
+    # .item_ranked_by_total_revenue_generated(params[:quantity])
     respond_with most_revenue
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,8 @@ class Item < ApplicationRecord
   def self.find_random_item
     order("RANDOM()").limit(1)
   end
+
+  def self.item_ranked_by_total_revenue_generated(quantity)
+    # joins(:invoice_items).group(:id).order("sum(invoice_items.quantity * invoice_items.unit_price) DESC").limit("#{quantity}")
+  end
 end
