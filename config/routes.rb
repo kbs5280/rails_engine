a2e819c2168485a210931aa2ef0c6e9ffaa4318d
Lane Winham <mlwinham12@gmail.com>
2016-09-07 18:33:03 -0600

    Add controller and routes for invoices invoice_items

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
         get 'find',     to: 'find#show'
         get 'random',   to: 'random#show'
         get '/:id/transactions', to: 'transactions#index'
+        get '/:id/invoice_items', to: 'invoice_items#index'
       end
         resources :invoices, only: [:index, :show]
 
