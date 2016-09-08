ac91a8c2c20f87eeed0cc07e907b3f406d58c0f9
Lane Winham <mlwinham12@gmail.com>
2016-09-08 10:47:22 -0600

    Add before action to items and items invoices. Add default scope to item

--- app/controllers/api/v1/invoice_items/find_controller.rb
+++ app/controllers/api/v1/invoice_items/find_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::InvoiceItems::FindController < ApplicationController
+  before_action :sanitize_unit_price
   respond_to :json, :xml
 
   def index
@@ -15,5 +16,11 @@ class Api::V1::InvoiceItems::FindController < ApplicationController
 
     def invoice_items_params
       params.permit(:id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at)
+    end
+
+    def sanitize_unit_price
+      if params[:unit_price] && params[:unit_price].include?(".")
+        params[:unit_price] = params[:unit_price].delete"."
+      end
     end
 end
