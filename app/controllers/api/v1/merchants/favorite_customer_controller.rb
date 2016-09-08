8ade62d634b0342c444610a9030a7ba60edb6141
Lane Winham <mlwinham12@gmail.com>
2016-09-08 15:45:56 -0600

    Pull query for merchant favorite customer into model and run against spec

--- app/controllers/api/v1/merchants/favorite_customer_controller.rb
+++ app/controllers/api/v1/merchants/favorite_customer_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::Merchants::FavoriteCustomerController < ApplicationController
   respond_to :json, :xml
 
   def show
-    Merchant.first.customers.select("customers.*, count(transactions.id) as transaction_count").joins(:transactions).where(transactions: { result: 'success' }).group('customers.id').order('transaction_count DESC').first
+    customer = Merchant.merchant_favorite_customer(params[:id])
+    respond_with customer
   end
 end
