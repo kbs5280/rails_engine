9b8ceed3e1a05259f69f4d2ccc2c0210947f4731
Lane Winham <mlwinham12@gmail.com>
2016-09-08 19:24:57 -0600

    Add test for show on all base controllers

--- spec/requests/api/v1/invoice_items_controller_spec.rb
+++ spec/requests/api/v1/invoice_items_controller_spec.rb
@@ -5,11 +5,23 @@ describe 'Invoice Items controller' do
   it 'sends a list of invoice items' do
     invoice_items = create_list(:invoice_item, 10)
 
-    get '/api/v1/invoice_items.json'
+    get '/api/v1/invoice_items'
 
     expect(response).to be_success
 
     expect(json.first["id"]).to eq(invoice_items.first.id)
     expect(json.count).to eq(10)
+  end
+
+  it 'returns a single invoice item with id' do
+    invoice_item = create(:invoice_item, id: 1)
+    invoice_item2 = create(:invoice_item, id: 2)
+
+    get "/api/v1/invoice_items/1"
+
+    expect(response).to be_success
+
+    expect(json["id"]).to eq 1
+    expect(json.count).to eq(7)
   end
 end
--- spec/requests/api/v1/items_controller_spec.rb
+++ spec/requests/api/v1/items_controller_spec.rb
@@ -8,8 +8,20 @@ describe 'Items controller' do
     get '/api/v1/items'
 
     expect(response).to be_success
-    
+
     expect(json.first["name"]).to eq(items.last.name)
     expect(json.count).to eq(10)
+  end
+
+  it 'return a single item with matching id' do
+    item = create(:item, id: 1 )
+    item2 = create(:item, id: 2, name: 'Jimmy' )
+
+    get "/api/v1/items/2"
+
+    expect(response).to be_success
+
+    expect(json["name"]).to eq "Jimmy"
+    expect(json.count).to eq(7)
   end
 end
